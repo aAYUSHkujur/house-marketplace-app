c821e2a252bd83305d4c50b51fc9d7cd7afec2f1
aayushk720@gmail.com <aayushk720@gmail.com>
2023-02-20 12:46:17 +0530

    added oauth

---
+++ src/components/OAuth.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { Toast } from "react-toastify";
+import googleIcon from "../assets/svg/googleIcon.svg";
+import { async } from "@firebase/util";
+
+function OAuth() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check for user
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      // If user doesn't exit , create user
+      if (!docSnap.exists()) {
+      }
+    } catch (error) {}
+  };
+
+  return (
+    <div className="socialLogin">
+      <p>Sign {location.pathname === "/sign-up" ? "up" : "in"} with </p>
+      <button className="socialIconDiv" onClick={onGoogleClick}>
+        <img className="socialIconImg" src={googleIcon} alt="google" />
+      </button>
+    </div>
+  );
+}
+
+export default OAuth;
