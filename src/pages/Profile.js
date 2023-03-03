89c0422534cb673aa6e3a2b0fccc3a26acee69be
aayushk720@gmail.com <aayushk720@gmail.com>
2023-03-03 23:54:55 +0530

    updated

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,8 +4,8 @@ import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { useNavigate, Link } from "react-router-dom";
 import { toast } from "react-toastify";
-import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
-import homeIcon from '../assets/svg/homeIcon.svg'
+import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
+import homeIcon from "../assets/svg/homeIcon.svg";
 
 function Profile() {
   const auth = getAuth();
@@ -50,7 +50,6 @@ function Profile() {
       [e.target.id]: e.target.value,
     }));
   };
-
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -94,9 +93,9 @@ function Profile() {
         </div>
 
         <Link to="/create-listing" className="createListing">
-          <img src={homeIcon} alt="home"/>
+          <img src={homeIcon} alt="home" />
           <p>Sell or rent your home</p>
-          <img src={arrowRight} alt="arrow right"/>
+          <img src={arrowRight} alt="arrow right" />
         </Link>
       </main>
     </div>
